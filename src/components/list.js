a10969b9088c932f194694c7a7a79c5c8c605872
Aiaru <aiaru.mukhamedyarova@gmail.com>
2024-09-05 11:41:52 +0900

    edit list

--- src/components/list.js
+++ src/components/list.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function ProjectList() {
   return (
-    <div className="project-list">
-      <ul>
-        {Array.from({ length: 50 }, (_, i) => (
-          <li key={i}>Project {i + 1}</li>
+    <div className="project-list flex text-left w-full">
+      <ul className="divide-y divide-blue-200 w-full">
+        {Array.from({ length: 20 }, (_, i) => (
+          <li key={i} className="p-3">
+            <p className="text-3xl">Project {i + 1}</p>
+          </li>
         ))}
       </ul>
     </div>
