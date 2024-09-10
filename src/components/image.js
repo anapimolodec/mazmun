c1597fd03aa04f0897618e560e35cc2661d7f135
Aiaru <aiaru.mukhamedyarova@gmail.com>
2024-09-10 15:23:34 +0900

    works well images

--- src/components/image.js
+++ src/components/image.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImagePlacement from "./random";
 
 const ImageBox = ({ modal, projects }) => {
   const { active, index } = modal;
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    setKey((prevKey) => prevKey + 1);
+  }, [index]);
+
   const projectImages = active && projects[index] ? projects[index].src : [];
-  return <ImagePlacement images={projectImages} />;
+
+  return <ImagePlacement key={key} images={projectImages} />;
 };
 
 export default ImageBox;
