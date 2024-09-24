162204bfd0e195e7f9b25657d6d1ba2428997b88
Aiaru <aiaru.mukhamedyarova@gmail.com>
2024-09-24 16:14:50 +0900

    fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ function Home() {
   const { t } = useTranslation();
   return (
     <Fade>
-      <div className="  ">
+      <div>
         <h1 className="text-8xl py-10 text-white ">{t("home")}</h1>
       </div>
     </Fade>
