6f626d40c52a637b071dc4f8d1f8647a46264263
Aiaru <aiaru.mukhamedyarova@gmail.com>
2024-09-24 16:14:15 +0900

    fix

--- src/components/mesh.js
+++ src/components/mesh.js
@@ -1,5 +1,4 @@
 import React, { useRef, useEffect, useState } from "react";
-import { useFrame } from "@react-three/fiber";
 import { useMotionValue, useSpring } from "framer-motion";
 import { motion } from "framer-motion-3d";
 import { Text } from "@react-three/drei";
